59189520b0238afd26dae88b749dc8d6c27113cd
Milad Asgari <jupit3r.milad@gmail.com>
2020-10-19 12:31:21 +0200

    add remove button on drag's Artikel

--- DragDrop.js
+++ DragDrop.js
@@ -11,6 +11,30 @@ const positionAtls = [
   { name: "artikel10", x: "", y: "" },
 ];
 
+
+const removeArtikel = (ev)=>{
+
+  const myList = document.getElementById('myList');
+  ev.target.style.visibility = 'hidden';
+  ev.target.parentElement.id
+  const findArrayinx = positionAtls.findIndex(
+    (ind) => ind.name == ev.target.parentElement.id
+  );
+  const artikel = positionAtls[findArrayinx];
+  artikel.name = ev.target.parentElement.id;
+  artikel.x = "";
+  artikel.y = "";
+
+  positionAtls[findArrayinx] = artikel;
+
+  myList.appendChild(ev.target.parentElement);
+  
+  
+  
+
+}
+
+
 function targetInfo(ev) {
   const findArrayinx = positionAtls.findIndex(
     (ind) => ind.name == ev.target.firstChild.id
@@ -41,71 +65,77 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+  
   ev.dataTransfer.setData("text", ev.target.id);
+  
+    
 }
 
 function dragStart(ev) {
-  //console.log(ev.currentTarget.id);
-  // const lastDiv = document.getElementById(ev.currentTarget.id);
-  // lastDiv.className = "defaultDiv row align-items-center px-0";
+  
 }
 
 function drop(ev) {
   ev.preventDefault();
-  if (ev.target.childNodes.length < 1) {
+  if (ev.target.childNodes.length < 1  ) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    
+    
+    ev.target.firstChild.firstElementChild.style.visibility = 'visible';
 
+
+    
     targetInfo(ev);
 
-    //console.log(ev.target.firstElementChild.innerHTML);
-    // switch (ev.currentTarget.lastChild.id) {
-    //   case "artikel1":
-    //     ev.currentTarget.className = "artikel1 row align-items-center px-0";
+    
+    switch (ev.currentTarget.lastChild.id) {
+      case "artikel1":
+        
+        
+        break;
 
-    //     break;
+      // case "artikel2":
+      //   ev.currentTarget.className = "artikel2 row align-items-center px-0";
 
-    //   case "artikel2":
-    //     ev.currentTarget.className = "artikel2 row align-items-center px-0";
+      //   break;
+      // case "artikel3":
+      //   ev.currentTarget.className = "artikel3 row align-items-center px-0";
 
-    //     break;
-    //   case "artikel3":
-    //     ev.currentTarget.className = "artikel3 row align-items-center px-0";
+      //   break;
+      // case "artikel4":
+      //   ev.currentTarget.className = "artikel4 row align-items-center px-0";
 
-    //     break;
-    //   case "artikel4":
-    //     ev.currentTarget.className = "artikel4 row align-items-center px-0";
+      //   break;
+      // case "artikel5":
+      //   ev.currentTarget.className = "artikel5 row align-items-center px-0";
 
-    //     break;
-    //   case "artikel5":
-    //     ev.currentTarget.className = "artikel5 row align-items-center px-0";
+      //   break;
+      //   case "artikel6":
+      //   ev.currentTarget.className = "artikel6 row align-items-center px-0";
 
-    //     break;
-    //     case "artikel6":
-    //     ev.currentTarget.className = "artikel6 row align-items-center px-0";
+      //   break;
+      //   case "artikel7":
+      //   ev.currentTarget.className = "artikel7 row align-items-center px-0";
 
-    //     break;
-    //     case "artikel7":
-    //     ev.currentTarget.className = "artikel7 row align-items-center px-0";
+      //   break;
+      //   case "artikel8":
+      //   ev.currentTarget.className = "artikel8 row align-items-center px-0";
 
-    //     break;
-    //     case "artikel8":
-    //     ev.currentTarget.className = "artikel8 row align-items-center px-0";
+      //   break;
+      //   case "artikel9":
+      //   ev.currentTarget.className = "artikel9 row align-items-center px-0";
 
-    //     break;
-    //     case "artikel9":
-    //     ev.currentTarget.className = "artikel9 row align-items-center px-0";
+      //   break;
+      //   case "artikel10":
+      //   ev.currentTarget.className = "artikel10 row align-items-center px-0";
 
-    //     break;
-    //     case "artikel10":
-    //     ev.currentTarget.className = "artikel10 row align-items-center px-0";
+      //   break;
 
-    //     break;
+      default:
 
-    //   default:
-
-    //     break;
-    // }
+        break;
+    }
   }
 }
 
@@ -130,8 +160,10 @@ const generateDiv = () => {
       const secondDiv = document.getElementById(`Xdiv${index}`);
       // const childInner = document.createTextNode("chetori");
       // divInner.appendChild(childInner);
-      const divInner = document.createElement("div");
+      
 
+
+      const divInner = document.createElement("div");
       divInner.id = `ydiv${ind}`;
       divInner.className = "defaultDiv";
 
@@ -141,7 +173,14 @@ const generateDiv = () => {
       divInner.ondrop = (event) => {
         drop(event);
       };
+
+     
+
+
       secondDiv.appendChild(divInner);
+      
+      
+     
     }
   }
 };
